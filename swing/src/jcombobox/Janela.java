71ed2979ff1e32c8848008eb6a7e6b87c7d9ff77
Rafael De Luca <deluca1712@gmail.com>
2021-11-15 12:12:02 -0300

    Exemplo Jlist

--- swing/src/jcombobox/Janela.java
+++ swing/src/jcombobox/Janela.java
@@ -12,16 +12,27 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
 
 public class Janela extends JFrame {
 
 
-	private JComboBox comboMaterias;
+
+	private static final long serialVersionUID = 1L;
+
+	//private JComboBox<String> comboMaterias;
+	@SuppressWarnings("rawtypes")
+	private JList listMaterias;
 	private JButton botaoTeste;
-	private JLabel labelEstados;
+	private JLabel labelMaterias;
 	private Icon[] icones;
 	private JLabel labelIcone;
+	
 
 	public Janela() {
 
@@ -35,39 +46,43 @@ public class Janela extends JFrame {
 	public void initComponents() {
 
 
-		labelEstados = new JLabel("Materia: ");
-		this.add(labelEstados);		
+		labelMaterias = new JLabel("Materia: ");
+		this.add(labelMaterias);		
 
-		String [] materias = {"Selecione um item...","Química", "Física", "Biologia"};
+		String [] materias = {"Selecione um item...","Química", "Física", "Biologia","História","Geografia"};
+		/*
 		comboMaterias = new JComboBox(materias);
 		comboMaterias.addItem("Matemática");
 		comboMaterias.addActionListener(new HandLerComboestado());
 		this.add(comboMaterias);
+		 */
+		
+		listMaterias = new JList(materias);		
+		this.add(listMaterias);
+		listMaterias.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);//selecionar apenas 1
+		//listMaterias.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);//selecionar range
+		listMaterias.addListSelectionListener(new HandlerListMaterias());
 
 
-		icones = new Icon[5];
+		icones = new Icon[6];
 		icones[0]= new ImageIcon("");
-		
-		
+
+
 		icones[1] = createImageIcon("c:\\imagem\\quimica.png","Química");
 		icones[2] = createImageIcon("c:\\imagem\\fisica.jpg","Física");
 		icones[3] = createImageIcon("c:\\imagem\\biologia.png","Biologia");
 		icones[4] = createImageIcon("c:\\imagem\\matematica.jpg","Matemática");
-		
-		/*
-		
+
+		/*		
 		icones[1]= new ImageIcon(this.getClass().getClassLoader().getResource("quimica.png"));		
 		icones[2]= new ImageIcon(this.getClass().getResource("c:\\imagem\\fisica.jpg"));
 		icones[3]= new ImageIcon(this.getClass().getResource("c:\\imagem\\biologia.png"));
 		icones[4]= new ImageIcon(this.getClass().getResource("c:\\imagem\\matematica.jpj"));
-		*/
-		
+		 */
+
 
 		labelIcone = new JLabel();
-		this.add(labelIcone);		
-
-
-
+		this.add(labelIcone);
 
 		botaoTeste = new JButton("Imprimir");
 		botaoTeste.addActionListener(new HandLerBotaoTeste());
@@ -79,17 +94,42 @@ public class Janela extends JFrame {
 		this.setBounds(100, 100, 450, 200);
 		//this.setSize(500,400);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setTitle("Teste de JComboBox");
+		this.setTitle("Teste de JList");
 		this.setLocationRelativeTo(null); //fazer aparecer no centro
 		this.setLayout(new FlowLayout());		
 
 	}
+	
+	private class HandlerListMaterias implements ListSelectionListener {
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			int id = listMaterias.getSelectedIndex();
+			labelIcone.setIcon(icones[id]);
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
 
 	private class HandLerBotaoTeste implements ActionListener {
 
+		@SuppressWarnings("deprecation")
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			JOptionPane.showMessageDialog(null, "Matéria: " + comboMaterias.getSelectedItem() +"\nÍndice da matéria: " + comboMaterias.getSelectedIndex());
+			Object[] objetos = listMaterias.getSelectedValues();
+			for(Object apelido: objetos) {
+				System.out.println(apelido);
+			}
+			
+			/*
+			JOptionPane.showMessageDialog(null,"Índice da Matéria: " + listMaterias.getSelectedIndex() + "\nMatéria: "
+			+ listMaterias.getSelectedValue());
+			*/
 
 		}
 
@@ -111,7 +151,7 @@ public class Janela extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			//System.out.println(comboMaterias.getSelectedItem());
-			int id = comboMaterias.getSelectedIndex();
+			int id = listMaterias.getSelectedIndex();
 			labelIcone.setIcon(icones[id]);
 
 		}
